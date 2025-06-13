643e83a2db56520361f131a1f5bc417bd96e4a98
RamadhaniRi <ramadhani9112@gmail.com>
2025-06-13 13:19:24 +0700

    angka genap

---
+++ python/3_loop_cetakAngka_satuBaris.py
@@ -0,0 +1,5 @@
+#kita ingin mencetak angka satu baris tidak berbaris-baris
+# carannya pakai end=" "
+
+for i in range(6):
+    print(i, end=" ")
---
+++ python/4_cetak_angka_genap.py
@@ -0,0 +1,14 @@
+# Pakai For loop
+print("==== For Loop ====")
+
+for i in range(2, 21, 2): #kita akan mencetak angka genap dari 2 sampai 20, kita pakai step 2 agar +2
+    print(i)
+
+# Pakai While loop
+print("==== While Loop ====")
+
+i = 2
+
+while i <= 20: #selama i kurang atau sama dengan 20
+    print(i)
+    i += 2 #i + 2 agar hasilnya genap
