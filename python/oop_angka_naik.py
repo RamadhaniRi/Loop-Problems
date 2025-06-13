9a79bab306c22782060475476ce032f8c3cda3e8
RamadhaniRi <ramadhani9112@gmail.com>
2025-06-13 13:09:51 +0700

    loop angka naik

---
+++ python/oop_angka_naik.py
@@ -0,0 +1,18 @@
+# Pakai For loop
+print("==== For Loop ====")
+
+for i in range(1, 4):
+    print(i)
+    #jadi karena range nya (1, 11) maka python akan loop angka 1 sampai 3
+
+
+# Pakai While loop
+print("==== While Loop ====")
+
+i = 1 #kita buat dulu i untuk sebagai angka mulai nya
+while i <= 3: #selama i kurang atau sama dengan 3 maka loop
+    print(i) #print duluan
+    i += 1 #i ditambah 1 setiap loop
+
+# ketika i = 3 maka i akan diprint dahulu akan tetapi ketika i = 3 ditambah 1 jadi 4
+# maka i tidak aka bisa di ptint lagi karena i lebih dari 3
