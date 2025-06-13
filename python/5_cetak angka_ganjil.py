f3ac21c7846cecc0ac8f05573ff2e55eee455fd9
RamadhaniRi <ramadhani9112@gmail.com>
2025-06-13 13:24:04 +0700

    cetak angka ganjil

---
+++ python/5_cetak angka_ganjil.py
@@ -0,0 +1,14 @@
+# Pakai For loop
+print("==== For Loop ====")
+
+for i in range(1, 22, 2): #kita akan mencetak angka ganji; dari 1 sampai 21, kita pakai step 2 agar +2
+    print(i)
+
+# Pakai While loop
+print("==== While Loop ====")
+
+i = 1
+
+while i <= 21: #selama i kurang atau sama dengan 21
+    print(i, end=",") #agar inputnya satu bari dan dipisahkan dengan koma tiap angka
+    i += 2 #i + 2 agar hasilnya ganjil
