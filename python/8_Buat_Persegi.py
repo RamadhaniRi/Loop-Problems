63d67d2c9b89d5346aaeb595be3790d1cfe2a864
RamadhaniRi <ramadhani9112@gmail.com>
2025-06-14 14:04:34 +0700

    buat persegi

---
+++ python/8_Buat_Persegi.py
@@ -0,0 +1,8 @@
+# kita akan membuat persegi
+# caranya kita pakai for loop
+# kemudian baris dan kolom jumlahnya harus sama
+# disini kita pakai 5 kolom dan 5 baris
+for i in range(5):
+    for i in range(5):
+        print(".", end=" ")
+    print()
