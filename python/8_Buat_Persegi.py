a0de66eeb5dc59ea64e7c22135b1c12de17ceb92
RamadhaniRi <ramadhani9112@gmail.com>
2025-06-14 14:09:29 +0700

    Buat persegi

--- python/8_Buat_Persegi.py
+++ python/8_Buat_Persegi.py
@@ -2,7 +2,25 @@
 # caranya kita pakai for loop
 # kemudian baris dan kolom jumlahnya harus sama
 # disini kita pakai 5 kolom dan 5 baris
+
+print("for loop")
+
 for i in range(5):
     for i in range(5):
         print(".", end=" ")
+    print()
+
+print()
+print()
+
+
+print("while loop")
+
+j = 0
+while j < 5:
+    j += 1
+    i = 0
+    while i < 5:
+        print("#", end=" ")
+        i += 1
     print()
